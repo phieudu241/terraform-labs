375694cada530d72ab4de2678588b8b519ab1b7c
Dung Le <phieudu241@gmail.com>
2024-04-26 15:52:39 +0700

    feat: create S3 instance

---
+++ s3/main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_version = ">=0.13.0"
+  required_providers {
+    aws = {
+      version = ">= 2.7.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "ap-northeast-1"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "my_project" {
+  bucket = "terraform-bucket-dunglq"
+
+  tags = {
+    Name        = "terraform bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "my_project" {
+  bucket = aws_s3_bucket.my_project.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "my_project" {
+  bucket = aws_s3_bucket.my_project.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "my_project" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.my_project,
+    aws_s3_bucket_public_access_block.my_project,
+  ]
+
+  bucket = aws_s3_bucket.my_project.id
+  acl    = "public-read"
+}
